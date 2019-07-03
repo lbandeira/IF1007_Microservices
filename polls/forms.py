610fff4482b768e98a113ac5337500a6572341ef
lbandeira <lbms@cin.ufpe.br>
2019-07-03 08:40:59 -0300

    add cadastro

---
+++ polls/forms.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.forms import User
+from .models import usuario
+from django import forms
+
+class UserForm(forms.ModelForm):
+	password = forms.CharField(widget=forms.PasswordInput)
+
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password']
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
-def login(request):
+def cadastro(request):
     return render(request, 'polls/login.html', {})
+
+def test(request):
+    return render(request, 'polls/test.html', {})
+
+
+
+def login(request):
+    username = request.POST.get('username', False)
+    password = request.POST.get('password', False)
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        #login(request, user)
+        # Redirect to a success page.
+        return render(request, 'polls/test.html', {})
+    else:
+        # Return an 'invalid login' error message.
+        return render(request, 'polls/login.html', {})
+        
